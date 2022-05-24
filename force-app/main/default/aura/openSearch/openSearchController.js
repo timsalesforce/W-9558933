1a205da9093f5c18ea076d7e567eff45709e7a41
Timothy <tdionne@salesforce.com>
2022-05-24 16:56:17 -0700

    init

---
+++ force-app/main/default/aura/openSearch/openSearchController.js
@@ -0,0 +1,9 @@
+({
+    openTab : function(component) {
+        var workspaceAPI = component.find("workspace");
+        workspaceAPI.openTab({
+            url: '/one/one.app#eyJjb21wb25lbnREZWYiOiJmb3JjZVNlYXJjaDpzZWFyY2giLCJhdHRyaWJ1dGVzIjp7InRlcm0iOiIxMTExMTExMTEzIiwic2NvcGVNYXAiOnsicmVzdWx0c0NtcCI6ImZvcmNlU2VhcmNoOnJlc3VsdHNUb3BSZXN1bHRzIiwibGFiZWwiOiJUb3AgUmVzdWx0cyIsInR5cGUiOiJUT1BfUkVTVUxUUyIsImNhY2hlYWJsZSI6IlkiLCJpZCI6IlRPUF9SRVNVTFRTIiwibGFiZWxQbHVyYWwiOiJUb3AgUmVzdWx0cyJ9LCJjb250ZXh0Ijp7ImRpc2FibGVTcGVsbENvcnJlY3Rpb24iOmZhbHNlLCJTRUFSQ0hfQUNUSVZJVFkiOnsidGVybSI6MTIzNDU2Nzg5MH19fSwic3RhdGUiOnt9fQ%3D%3D',
+            focus: true
+        });
+    }
+})
